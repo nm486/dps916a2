67ba534ac3f70f67a90dbab9063402909b1abd62
Raymond Hung <hung.raymond@gmail.com>
2013-04-19 20:31:04 -0400

    Added tests for address and email

--- DPS916-Assignment2/A2MVCApplicationTests/Controllers/AddressControllerTest.vb
+++ DPS916-Assignment2/A2MVCApplicationTests/Controllers/AddressControllerTest.vb
@@ -4,19 +4,106 @@ Imports System.Text
 Imports System.Web.Mvc
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 Imports A2MVCApplication.A2MVCApplication
+Imports A2MVCApplication
 
 <TestClass()> Public Class AddressControllerTest
 
-    <TestMethod()> Public Sub Index()
-        ' Arrange
-        Dim controller As New AddressController()
+    Dim controller As AddressController
+    Dim testAddress As A2MVCApplication.A2Models.AddressModel
 
-        ' Act
-        Dim result As ViewResult = DirectCast(controller.Index(), ViewResult)
+    Dim db As New AddressBookContext
 
-        ' Assert
-        Dim viewData As ViewDataDictionary = result.ViewData
-        Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", viewData("Message"))
+    <TestInitialize()>
+    Public Sub Initialize()
+        AppDomain.CurrentDomain.SetData("DataDirectory", Environment.CurrentDirectory)
+        controller = New AddressController()
+        Dim testRecord As A2Models.RecordModel
+        Dim abController = New AddressBookController()
+        Dim rController = New RecordController()
+        Dim ab = New A2Models.AddressBookModel()
+        ab.AddressBookName = "Test Book"
+        abController.Create(ab)
+        Dim tempAB = db.AddressBooks.FirstOrDefault(Function(t) t.AddressBookName = "Test Book")
+        testRecord = New A2Models.RecordModel()
+        testRecord.UserName = "Test User Address"
+        testRecord.AddressBookId = tempAB.AddressBookId
+        rController.Create(testRecord)
     End Sub
 
+    <TestCleanup>
+    Public Sub CleanUp()
+        controller = Nothing
+    End Sub
+
+    Private Sub ReseedDatabase()
+        Entity.Database.SetInitializer(New DatabaseInitializer("../../App_Data/seedDB.jab"))
+    End Sub
+
+    <TestMethod()>
+    Public Sub Create_ValidAddressTest()
+        ReseedDatabase()
+        testAddress = New A2Models.AddressModel()
+        testAddress.Text = "70 The Pond Road"
+        testAddress.RecordId = db.Records.First().RecordId
+
+        Assert.IsTrue(testAddress.ModelState.IsValid())
+        Dim result = controller.Create(testAddress)
+        Assert.IsNotNull(result)
+        Dim createdAddress As A2Models.AddressModel = db.Addresses.FirstOrDefault(Function(t) t.Text = "70 The Pond Road")
+        Assert.AreEqual(createdAddress.Text, testAddress.Text)
+        Assert.IsTrue(createdAddress.ModelState.IsValid())
+    End Sub
+
+    <TestMethod()>
+    Public Sub Create_InvalidAddressTest()
+        ReseedDatabase()
+        testAddress = New A2Models.AddressModel()
+        testAddress.Text = "70! The Pond Road"
+        testAddress.RecordId = db.Records.First().RecordId
+        Assert.IsFalse(testAddress.ModelState.IsValid())
+        Dim result = DirectCast(DirectCast(controller.Create(testAddress), ViewResult).Model, A2Models.AddressModel)
+        Assert.IsFalse(result.ModelState.IsValid())
+    End Sub
+
+    <TestMethod()>
+    Public Sub Edit_ValidAddressTest()
+        ReseedDatabase()
+        testAddress = db.Addresses.FirstOrDefault()
+        Dim id = testAddress.AddressId
+        testAddress.Text = "1800 Testing Road"
+        controller.Edit(testAddress)
+
+        Dim result = DirectCast(DirectCast(controller.Details(id), ViewResult).Model, A2Models.AddressModel)
+
+        Assert.IsNotNull(result)
+        Assert.AreEqual(testAddress.Text, result.Text)
+    End Sub
+
+    <TestMethod()>
+    Public Sub Edit_InvalidAddressTest()
+        ReseedDatabase()
+        testAddress = db.Addresses.FirstOrDefault()
+        Dim id = testAddress.AddressId
+        Dim check = testAddress.Text
+        testAddress.Text = "1800@ Testing Road"
+        controller.Edit(testAddress)
+
+        Dim result = DirectCast(DirectCast(controller.Details(id), ViewResult).Model, A2Models.AddressModel)
+        Assert.AreEqual(result.Text, check)
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub Delete_AddressTest()
+        ReseedDatabase()
+        testAddress = New A2Models.AddressModel()
+        testAddress.Text = "24 Sussex Drive"
+        testAddress.RecordId = db.Records.First().RecordId
+        controller.Create(testAddress)
+        Dim pRecordId = db.Addresses.FirstOrDefault(Function(t) t.Text = testAddress.Text).RecordId
+        controller.Delete(pRecordId)
+        controller.DeleteConfirmed(pRecordId)
+        Dim result = controller.Details(pRecordId)
+        Assert.IsTrue(TypeOf (result) Is HttpNotFoundResult)
+    End Sub
 End Class
--- DPS916-Assignment2/A2MVCApplicationTests/Controllers/EmailControllerTest.vb
+++ DPS916-Assignment2/A2MVCApplicationTests/Controllers/EmailControllerTest.vb
@@ -4,19 +4,104 @@ Imports System.Text
 Imports System.Web.Mvc
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 Imports A2MVCApplication.A2MVCApplication
+Imports A2MVCApplication
 
 <TestClass()> Public Class EmailControllerTest
 
-    <TestMethod()> Public Sub Index()
-        ' Arrange
-        Dim controller As New EmailController()
+    Dim controller As EmailController
+    Dim testEmail As A2MVCApplication.A2Models.EmailModel
+    Dim testRecord As A2Models.RecordModel
+    Dim db As New AddressBookContext
 
-        ' Act
-        Dim result As ViewResult = DirectCast(controller.Index(), ViewResult)
-
-        ' Assert
-        Dim viewData As ViewDataDictionary = result.ViewData
-        Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", viewData("Message"))
+    <TestInitialize()>
+    Public Sub Initialize()
+        AppDomain.CurrentDomain.SetData("DataDirectory", Environment.CurrentDirectory)
+        controller = New EmailController()
+        If (testRecord Is Nothing) Then
+            Dim abController = New AddressBookController()
+            Dim rController = New RecordController()
+            Dim ab = New A2Models.AddressBookModel()
+            ab.AddressBookName = "Test Book"
+            abController.Create(ab)
+            Dim tempAB = db.AddressBooks.FirstOrDefault(Function(t) t.AddressBookName = "Test Book")
+            testRecord = New A2Models.RecordModel()
+            testRecord.UserName = "Test User Email"
+            testRecord.AddressBookId = tempAB.AddressBookId
+            rController.Create(testRecord)
+            testRecord = db.Records.FirstOrDefault(Function(t) t.UserName = "Test User")
+        End If
     End Sub
 
+    <TestCleanup>
+    Public Sub CleanUp()
+        controller = Nothing
+    End Sub
+
+    Private Sub ReseedDatabase()
+        Entity.Database.SetInitializer(New DatabaseInitializer("../../App_Data/seedDB.jab"))
+    End Sub
+
+    <TestMethod()>
+    Public Sub Create_ValidEmailTest()
+        testEmail = New A2Models.EmailModel()
+        testEmail.Text = "testblahblah@seneca.ca"
+        testEmail.RecordId = testRecord.RecordId
+
+        Assert.IsTrue(testEmail.ModelState.IsValid())
+        Dim result = controller.Create(testEmail)
+        Assert.IsNotNull(result)
+        Dim createdEmail As A2Models.EmailModel = db.Emails.FirstOrDefault(Function(t) t.Text = "testblahblah@seneca.ca")
+        Assert.AreEqual(createdEmail.Text, testEmail.Text)
+        Assert.IsTrue(createdEmail.ModelState.IsValid())
+    End Sub
+
+    <TestMethod()>
+    Public Sub Create_InvalidEmailTest()
+        testEmail = New A2Models.EmailModel()
+        testEmail.Text = "Hello@seneca.eu"
+        testEmail.RecordId = testRecord.RecordId
+        Assert.IsFalse(testEmail.ModelState.IsValid())
+        Dim result = DirectCast(DirectCast(controller.Create(testEmail), ViewResult).Model, A2Models.EmailModel)
+        Assert.IsFalse(result.ModelState.IsValid())
+    End Sub
+
+    <TestMethod()>
+    Public Sub Edit_ValidEmailTest()
+        testEmail = db.Emails.FirstOrDefault()
+        Dim id = testEmail.EmailId
+        testEmail.Text = "validemail@seneca.com"
+        controller.Edit(testEmail)
+
+        Dim result = DirectCast(DirectCast(controller.Details(id), ViewResult).Model, A2Models.EmailModel)
+
+        Assert.IsNotNull(result)
+        Assert.AreEqual(testEmail.Text, result.Text)
+    End Sub
+
+    <TestMethod()>
+    Public Sub Edit_InvalidEmailTest()
+        testEmail = db.Emails.FirstOrDefault()
+        Dim id = testEmail.EmailId
+        Dim check = testEmail.Text
+        testEmail.Text = "invalidemail@seneca.eu"
+        controller.Edit(testEmail)
+
+        Dim result = DirectCast(DirectCast(controller.Details(id), ViewResult).Model, A2Models.EmailModel)
+        Assert.AreEqual(result.Text, check)
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub Delete_EmailTest()
+        ReseedDatabase()
+        testEmail = New A2Models.EmailModel()
+        testEmail.Text = "deletemepls@seneca.org"
+        testEmail.RecordId = db.Records.FirstOrDefault().RecordId
+        controller.Create(testEmail)
+        Dim pRecordId = db.Emails.FirstOrDefault(Function(t) t.Text = "deletemepls@seneca.org").RecordId
+        controller.Delete(pRecordId)
+        controller.DeleteConfirmed(pRecordId)
+        Dim result = controller.Details(pRecordId)
+        Assert.IsTrue(TypeOf (result) Is HttpNotFoundResult)
+    End Sub
 End Class
